419c954561065bdf3a8523826a5f336f49964664
Chiboubnour <chiboubnour32@gmail.com>
2024-09-30 10:48:09 +0200

    Update popcount_u64.cpp

--- src/popcount_u64.cpp
+++ src/popcount_u64.cpp
@@ -2,33 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <ap_int.h>
-/*
-ap_uint<64> popcount_u64_c(const ap_uint<64> val) {//version 1
 
-    ap_uint<64> count = 0;
-    for (int i = 0; i < 64; ++i) {
-      #pragma HLS UNROLL
-        if (val[i]) {
-            count++;
-        }
-    }
-
-    return count;
-}
-
-ap_uint<64> popcount_u64_c(const ap_uint<64> _val) //version2
-{
-	ap_uint<64> x = _val;
-    int count = 0;
-    while ( x ) {
-        count += (x & 1);
-        x >>= 1;
-    }
-    return count;
-}
-
-*/
-ap_uint<7> popcount_u64_c(const ap_uint<64> in) {//version 4
+ap_uint<7> popcount_u64_c(const ap_uint<64> in) {
 
     ap_uint<7> out = 0;
     for (int i = 0; i < 64; ++i) {
@@ -38,8 +13,6 @@ ap_uint<7> popcount_u64_c(const ap_uint<64> in) {//version 4
 
     return out ;
 }
-
-
 
 
 
